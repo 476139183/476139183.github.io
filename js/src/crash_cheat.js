4735a4dfd26ab5e85b0ba3e66613dfdec84f1030
Yutian Duan <476139183@qq.com>
2018-09-07 17:26:59 +0800

    Site updated: 2018-09-07 17:26:58

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,18 +1,17 @@
-
 <!--崩溃欺骗-->
- var OriginTitle = document.title;
+var OriginTitile = document.title;
  var titleTime;
  document.addEventListener('visibilitychange', function () {
-     if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/img/TEP.ico");
-         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
-         clearTimeout(titleTime);
-     }
-     else {
-         $('[rel="icon"]').attr('href', "/favicon.ico");
-         document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitle;
-         titleTime = setTimeout(function () {
-             document.title = OriginTitle;
-         }, 2000);
-     }
- });
+ if (document.hidden) {
+     $('[rel="icon"]').attr('href', "/img/TEP.ico");
+     document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+     clearTimeout(titleTime);
+ }
+ else {
+     $('[rel="icon"]').attr('href', "/favicon.ico");
+     document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitile;
+     titleTime = setTimeout(function () {
+         document.title = OriginTitile;
+     }, 2000);
+ }
+});
